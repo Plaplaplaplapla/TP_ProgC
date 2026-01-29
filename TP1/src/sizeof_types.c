d80b2d88c062cabc8c2782c318ccc577e41a953f
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:01:59 +0100

    Add sizeof_types.c to display sizes of data types

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("%zu", sizeof(char));
+    printf("%zu", sizeof(short));
+    printf("%zu", sizeof(int));
+    printf("%zu", sizeof(long int));
+    printf("%zu", sizeof(long long int));
+    printf("%zu", sizeof(float));
+    printf("%zu", sizeof(double));
+    printf("%zu", sizeof(long double));
+
+    return 0;
+}
 
