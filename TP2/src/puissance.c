f368bdf55f9cd9efe58556feb7fde60d39e3821e
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:52:03 +0100

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2; 
+    int b = 3; 
+    int resultat = 1; 
+
+    int i;
+    for(i = 1; i <= b; i++) {
+        resultat = resultat * a;
+    }
+
+    printf("%d élevé à la puissance %d = %d\n", a, b, resultat);
+
+    return 0;
+}
