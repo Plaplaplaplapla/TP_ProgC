490d561e1f1a6ab2639ea21675d6cd19ecc70cce
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:05:33 +0100

    Add variables and print statements in variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char c = 'A';
+    int i = 10;
+    float f = 3.14;
+    double d = 2.718;
+
+    printf("char : %c\n", c);
+    printf("int : %d\n", i);
+    printf("float : %f\n", f);
+    printf("double : %lf\n", d);
+
+    return 0;
+}
