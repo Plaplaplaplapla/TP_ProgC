ce2819fc7db353d43d03ceede05ecb0cf26f2b9c
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:23:40 +0100

    Implement basic calculator in calculs.c
    Added a basic calculator implementation using switch-case to handle different operations.

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 3;
+    char op = '+';
+    int resultat;
+
+    switch(op) {
+        case '+':
+            resultat = num1 + num2;
+            break;
+        case '-':
+            resultat = num1 - num2;
+            break;
+        case '*':
+            resultat = num1 * num2;
+            break;
+        case '/':
+            resultat = num1 / num2;
+            break;
+        case '%':
+            resultat = num1 % num2;
+            break;
+        case '&':
+            resultat = num1 & num2;
+            break;
+        case '|':
+            resultat = num1 | num2;
+            break;
+        case '~':
+            resultat = ~num1;
+            break;
+        default:
+            printf("Opérateur inconnu\n");
+            return 0;
+    }
+
+    printf("%d\n", resultat);
+
+    return 0;
+}
 
