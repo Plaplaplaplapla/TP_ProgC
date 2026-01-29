8e1f83bc51eca4e4244e71e466de9f743cff835e
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:10:40 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,19 @@
 
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%d\n", a / b);
+    printf("%d\n", a % b);
+
+    printf("%d\n", a == b);
+    printf("%d\n", a > b);
+
+    return 0;
+}
 
