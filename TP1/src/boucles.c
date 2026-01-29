291958bbda5529d8df057dd0dd4f463aa9e4cf23
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-01-29 10:17:50 +0100

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5; 
+
+    for(int i = 1; i <= compteur; i++) {   
+        for(int j = 1; j <= i; j++) {        
+            if(i % 2 == 0 && j % 2 == 0)      
+                printf("# ");
+            else
+                printf("* ");
+        }
+        printf("\n");                          
+    }
